4b20e7ec91965a14e707664f9465dc8d9930db33
davidwanderson10 <profdavidwanderson@gmail.com>
2026-04-07 21:01:42 -0300

    atualizacao doc swagger

--- 4 - Node/Aula 4/api-biblioteca/swagger.js
+++ 4 - Node/Aula 4/api-biblioteca/swagger.js
@@ -1,5 +1,10 @@
 import swaggerUi from 'swagger-ui-express';
 import { autoresSchema, autoresPaths } from './src/schemas/autores.js';
+import { categoriasSchema, categoriasPaths } from './src/schemas/categorias.js';
+import { livrosSchema, livrosPaths } from './src/schemas/livros.js';
+import { loginSchema, loginPaths } from './src/schemas/login.js';
+import { usuariosSchema, usuariosPaths } from './src/schemas/usuarios.js';
+import { pedidosSchema, pedidosPaths } from './src/schemas/pedidos.js';
 
 const swaggerDocument = {
   openapi: '3.0.0',
@@ -26,17 +31,24 @@ const swaggerDocument = {
       Erro: {
         type: 'object',
         properties: {
-          error: {
-            type: 'string',
-            example: 'Mensagem de erro',
-          },
+          error: { type: 'string', example: 'Mensagem de erro' },
         },
       },
       ...autoresSchema,
+      ...categoriasSchema,
+      ...livrosSchema,
+      ...loginSchema,
+      ...usuariosSchema,
+      ...pedidosSchema,
     },
   },
   paths: {
+    ...loginPaths,
     ...autoresPaths,
+    ...categoriasPaths,
+    ...livrosPaths,
+    ...usuariosPaths,
+    ...pedidosPaths,
   },
 };
 
